ec844db9948365bb44f4df9f2c64a16d624218a4
Don Barlow <ottobar@gmail.com>
2019-02-11 14:00:29 -0700

    Initial commit

---
+++ lib/ex_aws/auto_scaling.ex
@@ -0,0 +1,18 @@
+defmodule ExAws.AutoScaling do
+  @moduledoc """
+  Documentation for ExAws.AutoScaling.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAws.AutoScaling.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aws/auto_scaling_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAws.AutoScalingTest do
+  use ExUnit.Case
+  doctest ExAws.AutoScaling
+
+  test "greets the world" do
+    assert ExAws.AutoScaling.hello() == :world
+  end
+end
